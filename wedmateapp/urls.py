4bd056a04cd2a4046a314ec7239918aca36746e1
amal c s <amalcs0006@gmail.com>
2023-12-09 12:56:27 +0530

    page cration

---
+++ wedmateapp/urls.py
@@ -0,0 +1,23 @@
+
+from django.contrib import admin
+from django.urls import re_path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from wedmateapp import views
+
+
+urlpatterns = [
+    re_path('admin/', admin.site.urls),
+    re_path(r'^$',views.base, name='base'),
+    re_path(r'^wedmate_quatationpdf$',views.wedmate_quatationpdf, name='wedmate_quatationpdf'),
+    # re_path(r'^books/(?P<id>\d+)/$',views.books, name='books'),  
+    
+
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- wedmateapp/views.py
+++ wedmateapp/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from wedmateapp.models import *
+import os
+from django.http import HttpResponse
+from datetime import datetime,date, timedelta
+from django.conf import settings
+from django.template.loader import get_template
+from xhtml2pdf import pisa
+import qrcode
+from django.core.files import File
 
-# Create your views here.
+def base(request):
+    return render(request,'base.html')
+
+def wedmate_quatationpdf(request):
+    
+    
+    template_path = 'wedmate_quatation.html'
+    context = {
+    'media_url':settings.MEDIA_URL,
+    }
+    # Create a Django response object, and specify content_type as pdf
+    response = HttpResponse(content_type='application/pdf')
+    #response['Content-Disposition'] = 'attachment; filename="certificate.pdf"'
+    response['Content-Disposition'] = 'filename="letter.pdf"'
+    # find the template and render it.
+    template = get_template(template_path)
+    html = template.render(context)
+
+    # create a pdf
+    pisa_status = pisa.CreatePDF(
+       html, dest=response)
+    
+
+
+    # if error then show some funy view
+    if pisa_status.err:
+       return HttpResponse('We had some errors <pre>' + html + '</pre>')
+    return response
+
+
